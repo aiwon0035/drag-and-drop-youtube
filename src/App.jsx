5787d6107816d3c0533cdcbf05f2f54ef13af5f1
aiwon0035 <OsakoSeiho@gmail.com>
2023-10-01 12:14:08 +0900

    drag-and-drop-youtube

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import './App.css'
+
+const items = [
+  {
+    id: "item-0",
+    content: "item 0",
+  },
+  {
+    id: "item-1",
+    content: "item 1",
+  },
+  {
+    id: "item-2",
+    content: "item 2",
+  },
+  {
+    id: "item-3",
+    content: "item 3",
+  },
+  {
+    id: "item-4",
+    content: "item 4",
+  },
+]
+
+const grid = 8;
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgray",
+  width: 250,
+  padding: grid,
+})
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: '0 0 ${grid} 0',
+  background: isDragging ? "lightgreen" : "gray",
+
+  ...draggableStyle,
+})
+
+const recorder = (list, startIndex, endIndex) => {
+  const removed = list.splice(startIndex, 1);
+  list.splice(endIndex, 0 ,removed[0]);
+}
+
+function App() {
+  const onDragEnd = (result) => {
+    if(!result.destination) {
+      return;
+    }
+    recorder(items, result.source.index, result.destination.index)
+  }
+
+  return (
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId='droppable'>
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+
+
+        </Droppable>
+      </DragDropContext>
+    </>
+  )
+}
+
+export default App
